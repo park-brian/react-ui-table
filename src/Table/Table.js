a7c405104fce8d380af367f7c97c48305b9925cc
park-brian <re.public@outlook.com>
2017-03-10 12:17:03 -0500

    initial commit: add project files

---
+++ src/Table/Table.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Table.css';
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <thead>
+        </thead>
+        <tbody>
+        </tbody>
+        <tfoot>
+        </tfoot>
+      </table>
+    );
+  }
+}
+
+export default Table;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Table from './Table/Table';
+import './index.css';
+
+ReactDOM.render(
+  <Table />,
+  document.getElementById('table')
+);
